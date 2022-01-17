5e134fe003dd392348967546e43410c60b858503
Elis Sandberg <reolreol@Eliss-MacBook-Air.local>
2022-01-17 11:57:00 +0300

    :construction:

--- messages/axelar/about/aboutAxelarMessageEng.js
+++ messages/axelar/about/aboutAxelarMessageEng.js
@@ -1,10 +1,10 @@
 const aboutAxelarMessageEng = () => {
   return `
-    👋 Hello and welcome to the Axelar Network community! 👋
+    👋 Welcome to the Axelar Network community! 👋
 
 Axelar is a scalable cross-chain platform that aims to connect multiple blockchains and networks with different languages, and provide a uniform solution to cross-chain communication that meets the needs of both platforms and developers.
 
-✅ Any new announcements regarding roadmap, new developments, etc. will be posted across our official announcement channels.
+Any new announcements regarding roadmap, new developments, etc. will be posted across our official announcement channels.
 `
 }
 
